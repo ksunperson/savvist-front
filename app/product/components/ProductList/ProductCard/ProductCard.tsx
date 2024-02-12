59981fad64b80424d69b3219010c68a23fc7c3ed
ksunperson <ksunperson@naver.com>
2024-02-12 19:56:23 +0900

    feat: ProductCard 컴포넌트에 반응형 그리드 레이아웃 구현

--- app/product/components/ProductList/ProductCard/ProductCard.tsx
+++ app/product/components/ProductList/ProductCard/ProductCard.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect, useState } from 'react';
 import ProductCardImage from './ProductCardImage';
 import ProductCardDescription from './ProductCardDescription';
 
@@ -9,8 +12,37 @@ export default function ProductCard() {
     { name: '모남희', description: '모남희 키링(화이트)', price: '83,000' },
   ];
 
+  const [numberOfColumns, setNumberOfColumns] = useState<number>(4);
+
+  useEffect(() => {
+    let resizeTimer: ReturnType<typeof setTimeout>;
+
+    const handleResize = () => {
+      // handleResize: 현재 창의 크기 가져와서 적절한 cols 값으로 설정
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        const width = window.innerWidth;
+        let cols = 4;
+        if (width < 768) {
+          cols = 2;
+        } else if (width < 1024) {
+          cols = 3;
+        }
+        setNumberOfColumns(cols);
+      }, 300);
+    };
+
+    handleResize(); // 초기 렌더링 시 한번 호출
+    window.addEventListener('resize', handleResize); // 리사이즈 이벤트 핸들러 등록
+
+    return () => {
+      window.removeEventListener('resize', handleResize); // 컴포넌트 언마운트 시 이벤트 핸들러 제거
+    };
+  }, []);
+
   return (
-    <div className="flex w-screen flex-row gap-20 items-center justify-center mt-20">
+    // grid-cols는 numberOfColumns에 따라 resizing
+    <div className={`flex w-screen flex-wrap gap-20 items-center justify-center mt-20 grid-cols-${numberOfColumns}`}>
       {productData.map((product, index) => (
         <div key={index} className="w-60">
           <ProductCardImage name={product.name} />
