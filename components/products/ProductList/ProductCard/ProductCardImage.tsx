4079e435c11d7d551a750370b6bd55f768382217
LSH <0867sh@naver.com>
2024-03-18 01:58:35 +0900

    상품 페이지 구현중

--- components/products/ProductList/ProductCard/ProductCardImage.tsx
+++ components/products/ProductList/ProductCard/ProductCardImage.tsx
@@ -1,17 +1,17 @@
 
 import Image from 'next/image';
-import 모남희 from '@/public/images/products/모남희.jpg';
 import Link from 'next/link';
 
 interface ProductCardImageProps {
   id?: string;
   name: string;
+  url: string;
 }
 
-export default function ProductCardImage({ id, name }: ProductCardImageProps) {
+export default function ProductCardImage({ id, name,url }: ProductCardImageProps) {
   return (
     <Link href={`/products/${id}`}>
-      <Image src={모남희} alt={name} />
+      <Image src={`http://localhost:5000/${url}`} width={300} height={300} alt={name} />
     </Link>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['localhost'], // localhost를 이미지 호스트로 추가
+      },
+};
 
 export default nextConfig;
+
