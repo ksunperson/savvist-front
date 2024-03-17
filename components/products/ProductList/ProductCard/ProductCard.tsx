148583cc9f128c4684555ac57c7128a452e3d624
LSH <0867sh@naver.com>
2024-03-18 00:28:20 +0900

    상품 페이지 구현중

--- components/products/ProductList/ProductCard/ProductCard.tsx
+++ components/products/ProductList/ProductCard/ProductCard.tsx
@@ -1,23 +1,27 @@
 "use client"
-
+import axios, { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
 import ProductCardImage from './ProductCardImage';
 import ProductCardDescription from './ProductCardDescription';
 
 export default function ProductCard() {
-  const productData = [
-    { id: 1, name: '모남희1', description: '모남희 키링(브레드)', price: '82,000' },
-    { id: 2, name: '모남희2', description: '모남희 키링(화이트)', price: '80,000' },
-    { id: 3, name: '모남희3', description: '모남희 키링(레드)', price: '90,000' },
-    { id: 4, name: '모남희4', description: '모남희 키링(화이트)', price: '83,000' },
-  ];
-
+  const [productData, setProductData] = useState<any[]>([]);
   const [numberOfColumns, setNumberOfColumns] = useState<number>(4);
 
   useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://localhost:5000/products');
+        console.log('응답 받음:', response.data);
+        setProductData(response.data);
+      } catch (error) {
+        console.error('오류 발생:', error);
+      }
+    }
+    fetchData();
+    
     let resizeTimer: ReturnType<typeof setTimeout>;
 
-    // handleResize: 현재 창의 크기 가져와서 적절한 cols 값으로 설정
     const handleResize = () => {
       clearTimeout(resizeTimer);
       resizeTimer = setTimeout(() => {
@@ -32,23 +36,22 @@ export default function ProductCard() {
       }, 300);
     };
 
-    handleResize(); // 초기 렌더링 시 한번 호출
-    window.addEventListener('resize', handleResize); // 리사이즈 이벤트 핸들러 등록
+    handleResize();
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize); // 컴포넌트 언마운트 시 이벤트 핸들러 제거
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    // grid-cols는 numberOfColumns에 따라 resizing
     <div className={`flex w-screen flex-wrap gap-20 items-center justify-center mt-20 grid-cols-${numberOfColumns}`}>
       {productData.map((product, index) => (
         <div key={index} className="w-60">
-          <ProductCardImage name={product.name} />
-          <ProductCardDescription name={product.name} description={product.description} price={product.price} />
+          <ProductCardImage name={product.title} url={product.img_url}/>
+          <ProductCardDescription name={product.title} description={product.description} price={product.price} />
         </div>
       ))}
     </div>
   );
-};
+}
